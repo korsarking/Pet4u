95166801791050ef6d730d4ce55f2e21f39860f7
Dumitru Samburi <korsarkings@gmail.com>
2025-10-06 15:13:40 +0300

    inserted Routes for login, register, loginPost, registerPost, logoutPost

--- pet4u_web/routes/web.php
+++ pet4u_web/routes/web.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GeneralController;
+use App\Http\Controllers\AuthController;
 
 Route::get("/", [GeneralController::class, "home"])->name("home");
 
 Route::get("/about", [GeneralController::class, "about"])->name("about");
 
 Route::get("/contacts", [GeneralController::class, "contacts"])->name("contacts");
+
+Route::get("/login", [AuthController::class, "login"])->name("login");
+
+Route::get("/register", [AuthController::class, "register"])->name("register");
+
+Route::post("/register", [AuthController::class, "registerPost"])->name("registerPost");
+
+Route::post("/login", [AuthController::class, "loginPost"])->name("loginPost");
+
+Route::post('/logout', [AuthController::class, 'logoutPost'])->name('logout');
